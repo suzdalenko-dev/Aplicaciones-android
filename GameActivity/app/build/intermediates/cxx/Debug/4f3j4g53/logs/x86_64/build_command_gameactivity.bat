4e2e6e2e886441af8290608fbd175ede861d10c0
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2024-07-10 14:23:23 +0200

    suzdalenko photo lapse 1.1.1.1.

--- GameActivity/app/build/intermediates/cxx/Debug/4f3j4g53/logs/x86_64/build_command_gameactivity.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\alexei\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Aplicaciones-android\\GameActivity\\app\\.cxx\\Debug\\4f3j4g53\\x86_64" ^
-  gameactivity
